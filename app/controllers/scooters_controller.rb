f50a6cad8e26c8f4cbad994c52a3ef7c9058894b
danthemanvsqz <danthemanvsqz@gmail.com>
2020-02-17 22:30:24 -0800

    add endpoint to deactivate scooter

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -1,6 +1,6 @@
 class ScootersController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :validate
+  before_action :validate, :only => [:update]
 
   def update
     scooter = Scooter.find(params[:id])
@@ -8,6 +8,11 @@ class ScootersController < ApplicationController
     scooter.update!(
       location: factory.point(params[:longitude], params[:latitude]),
       battery_level: params[:battery_level])
+    head :ok
+  end
+
+  def deactivate
+    Scooter.update(params[:id], inactive: true)
     head :ok
   end
 
--- test/controllers/scooters_controller_test.rb
+++ test/controllers/scooters_controller_test.rb
@@ -1,13 +1,23 @@
 require 'test_helper'
 
 class ScootersControllerTest < ActionDispatch::IntegrationTest
-  test "should put update" do
+  setup do
     factory = RGeo::Cartesian.factory
-    scooter = Scooter.find_or_create_by(location: factory.point(0,0), battery_level: 0, inactive: false)
-    put "/scooters/#{scooter.id}/update", params: {latitude: 5, longitude: 5, battery_level: 0.4}
-    assert_response :success
-    scooter.reload
-    assert_in_delta(scooter.battery_level, 0.4, 0.0001)
+    @scooter = Scooter.find_or_create_by(location: factory.point(0,0), battery_level: 0, inactive: false)
   end
+
+  test "should put update" do
+    put "/scooters/#{@scooter.id}/update", params: {latitude: 5, longitude: 5, battery_level: 0.4}
+    assert_response :success
+    @scooter.reload
+    assert_in_delta(@scooter.battery_level, 0.4, 0.0001)
+  end
+
+  test "should put deactivate" do
+    put "/scooters/#{@scooter.id}/deactivate"
+    assert_response :success
+    @scooter.reload
+    assert @scooter.inactive
+  end 
 
 end
